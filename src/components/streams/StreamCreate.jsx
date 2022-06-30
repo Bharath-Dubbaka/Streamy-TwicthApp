b0cb80d2e7870ae8f454bbed94a2944fc0322e55
Bharath Dubbaka <bharath.reactdev@gmail.com>
2022-06-30 14:36:18 +0530

    added redux-forms and wrote boiler plate code

--- src/components/streams/StreamCreate.jsx
+++ src/components/streams/StreamCreate.jsx
@@ -2,9 +2,40 @@ import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends Component {
+
+  renderInput = (formProps) => {
+    // console.log(formProps);
+    return (
+      <div className="field">
+        <label>{formProps.label}</label>
+        <input
+          {...formProps.input}
+          // onChange={formProps.input.onChange}
+          // value={formProps.input.value}
+        />
+      </div>
+    );
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault()
+  }
+
   render() {
-    return <div>Stre</div>;
+    console.log(this.props);
+    return (
+      <form className="ui form">
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Description"
+        />
+      </form>
+    );
   }
 }
 
-export default StreamCreate;
+export default reduxForm({
+  form: "streamCreate",
+})(StreamCreate);
